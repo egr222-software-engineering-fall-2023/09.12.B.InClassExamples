6829c21d01d6c3f403361212067da9d04e616c92
Larry Clement <lclement@calbaptist.edu>
2023-09-12 10:07:12 -0700

    After class

--- DrawFigures/src/DrawFigures.java
+++ DrawFigures/src/DrawFigures.java
@@ -1,22 +1,24 @@
 public class DrawFigures {
     public static void main(String[] args) {
+        drawDiamond();
+        drawX();
+        drawRocket();
+    }
+
+    private static void drawCone() {
         System.out.println("   /\\");
         System.out.println("  /  \\");
         System.out.println(" /    \\");
+    }
+
+    private static void drawV() {
         System.out.println(" \\    /");
         System.out.println("  \\  /");
         System.out.println("   \\/");
-        System.out.println();
-        System.out.println(" \\    /");
-        System.out.println("  \\  /");
-        System.out.println("   \\/");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
-        System.out.println();
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
+    }
+
+    public static void drawRocket() {
+        drawCone();
         System.out.println("+------+");
         System.out.println("|      |");
         System.out.println("|      |");
@@ -27,8 +29,17 @@ public class DrawFigures {
         System.out.println("|      |");
         System.out.println("|      |");
         System.out.println("+------+");
-        System.out.println("   /\\");
-        System.out.println("  /  \\");
-        System.out.println(" /    \\");
+        drawCone();
+    }
+
+    public static void drawDiamond() {
+        drawCone();
+        System.out.println();
+    }
+
+    public static void drawX() {
+        drawV();
+        drawCone();
+        System.out.println();
     }
 }
